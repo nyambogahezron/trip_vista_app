0552c22cfd009aeb67d0b4f1cf9d489c72baf390
Hezron Nyamboga <hezzyn1@gmail.com>
2024-07-07 03:00:13 +0300

    login page style update -- linear gradient

--- app/(auth)/Login.jsx
+++ app/(auth)/Login.jsx
@@ -1,50 +1,61 @@
 import React from 'react';
-import { View, Text, TextInput, Image, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
 import CustomButton from '../../components/CustomButton';
-import { router } from 'expo-router';
+import { router, Stack } from 'expo-router';
 import AuthHeader from '../../components/AuthHeader';
 import AuthFooter from '../../components/AuthFooter';
 
 const Login = () => {
   return (
-    <View className='flex-1 bg-white h-full'>
-      <AuthHeader title='Sign In' />
-      <View className='p-8'>
-        <Text className='text-2xl text-blue-800 font-pbold mt-4 text-center'>
-          Welcome To Back TripVista
-        </Text>
+    <SafeAreaView className='flex-1 bg-white h-full'>
+      <View className='flex-1'>
+        <AuthHeader title='Sign In' />
+        <View className='p-8'>
+          <Text className='text-xl bg-gradient-to-t from-sky-500 to-indigo-300 font-pbold mt-4 text-center'>
+            Welcome Back To TripVista
+          </Text>
 
-        <TextInput
-          className='mt-8 border-b-2 border-gray-300 pb-2'
-          placeholder='Email'
-          secureTextEntry
-        />
+          <TextInput
+            className='mt-8 border-b-2 border-gray-300 pb-2'
+            placeholder='Email'
+            secureTextEntry
+          />
 
-        <TextInput
-          className='mt-8 border-b-2 border-gray-300 pb-2'
-          placeholder='Password'
-          secureTextEntry
-        />
-        <TouchableOpacity className='mt-2'>
-          <Text className='text-blue-500'>Forgot Password?</Text>
-        </TouchableOpacity>
+          <TextInput
+            className='mt-8 border-b-2 border-gray-300 pb-2'
+            placeholder='Password'
+            secureTextEntry
+          />
+          <TouchableOpacity className='mt-2'>
+            <Text className='text-blue-500'>Forgot Password?</Text>
+          </TouchableOpacity>
 
-        <CustomButton
-          title='SIGN IN'
-          handlePress={() => router.push('/home')}
-          customStyles={
-            'border bg-blue-800 py-3 mt-8 rounded-full items-center'
-          }
-          textStyles={'text-white text-lg'}
-        />
+          <CustomButton
+            title='SIGN IN'
+            handlePress={() => router.push('/home')}
+            customStyles={
+              'border bg-blue-800 py-3 mt-8 rounded-full items-center'
+            }
+            textStyles={'text-white text-lg'}
+          />
 
-        <AuthFooter
-          title='Sign Up'
-          desc={`Don't Have An Account?`}
-          link='/register'
-        />
+          <AuthFooter
+            title='Sign Up'
+            desc={`Don't Have An Account?`}
+            link='/register'
+          />
+        </View>
       </View>
-    </View>
+      <StatusBar style='light-content' backgroundColor='transparent' />
+    </SafeAreaView>
   );
 };
 
--- components/AuthHeader.jsx
+++ components/AuthHeader.jsx
@@ -1,15 +1,23 @@
-import { View, Text, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { View, Text, Image} from 'react-native';
 const AuthHeader = ({ title }) => {
   return (
-    <View className='flex-1 bg-blue-800 rounded-b-3xl items-center'>
-      <Image
-        source={require('../assets/images/logo.png')}
-        className='w-16 h-16 mt-20 rounded-full'
-      />
-      <View className='mt-8'>
-        <Text className='text-white text-3xl font-pbold'>{title}</Text>
+    <>
+      <View className='relative flex-1 items-center'>
+        <LinearGradient
+          colors={['#2c5282', '#f4f4f4']}
+          className='absolute left-0 right-0 top-0 h-[300px] rounded-b-[35px] '
+        />
+        <Image
+          source={require('../assets/images/logo.png')}
+          className='w-16 h-16 mt-20 rounded-full'
+        />
+        <View className='mt-8'>
+          <Text className='text-white text-3xl font-pbold'>{title}</Text>
+        </View>
       </View>
-    </View>
+    </>
   );
 };
+
 export default AuthHeader;
